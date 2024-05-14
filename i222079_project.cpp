7d92a0b3bb2f5ae15e8aa9b9a057b479d3d6bfe3
ghazanfarghaffar <mghazanfarch@gmail.com>
2024-05-14 13:18:55 +0500

    working fine. but not sure for windows

--- i222079_project.cpp
+++ i222079_project.cpp
@@ -67,6 +67,109 @@ int minTrips(const vector<vector<Edge>> &adjList, int start, int end, int touris
 
 int main()
 {
+
+    int N, R, C1, C2, P, start, destination, tourists;
+
+    ofstream outputFile("data.txt");
+    if (!outputFile)
+    {
+        cerr << "Failed to open output file!" << endl;
+        return 1;
+    }
+
+    cout << "Enter the number of cities(0 FOR SAVED GRAPH): ";
+    cin >> N;
+    while (N < 0)
+    {
+
+        cerr << "Invalid input! Please enter a positive integer for the number of cities." << endl;
+        cin >> N;
+    }
+    if (N == 0)
+    {
+        goto label;
+    }
+    outputFile << N << " ";
+
+    while (true)
+    {
+        cout << "Enter the number of road segments: ";
+        if (!(cin >> R) || R < 0)
+        {
+            cerr << "Invalid input! Please enter a non-negative integer for the number of road segments." << endl;
+        }
+        else
+        {
+            break;
+        }
+    }
+    outputFile << R << " ";
+    outputFile << endl;
+
+    for (int i = 0; i < R; ++i)
+    {
+        while (true)
+        {
+            int C1, C2, P;
+            cout << "Enter the city pair and capacity of road segment " << i + 1 << ": ";
+            if (!(cin >> C1 >> C2 >> P) || C1 < 1 || C1 > N || C2 < 1 || C2 > N || P <= 0)
+            {
+                cerr << "Invalid input! Please enter valid city identifiers and positive capacities." << endl;
+            }
+            else
+            {
+                outputFile << C1 << " " << C2 << " " << P << " ";
+                outputFile << endl;
+                break;
+            }
+        }
+    }
+
+    while (true)
+    {
+        cout << "Enter the starting city: ";
+        if (!(cin >> start) || start < 1 || start > N)
+        {
+            cerr << "Invalid input! Please enter a valid city identifier for the starting city." << endl;
+        }
+        else
+        {
+            break;
+        }
+    }
+    outputFile << start << " ";
+
+    while (true)
+    {
+        cout << "Enter the destination city: ";
+        if (!(cin >> destination) || destination < 1 || destination > N)
+        {
+            cerr << "Invalid input! Please enter a valid city identifier for the destination city." << endl;
+        }
+        else
+        {
+            break;
+        }
+    }
+    outputFile << destination << " ";
+
+    while (true)
+    {
+        cout << "Enter the number of tourists: ";
+        if (!(cin >> tourists) || tourists < 0)
+        {
+            cerr << "Invalid input! Please enter a non-negative integer for the number of tourists." << endl;
+        }
+        else
+        {
+            break;
+        }
+    }
+    outputFile << tourists << " ";
+
+    outputFile.close();
+
+label:
     ifstream inputFile("data.txt");
     if (!inputFile)
     {
@@ -74,7 +177,6 @@ int main()
         return 1;
     }
 
-    int N, R;
     inputFile >> N >> R;
 
     vector<vector<Edge>> graph(N + 1); // 1-based indexing for cities
@@ -82,7 +184,6 @@ int main()
     // Input road segments
     for (int i = 0; i < R; ++i)
     {
-        int C1, C2, P;
         inputFile >> C1 >> C2 >> P;
         graph[C1].push_back({C2, P});
         graph[C2].push_back({C1, P}); // Since the graph is undirected
@@ -90,7 +191,6 @@ int main()
 
     printGraph(graph); // Print the constructed graph
 
-    int start, destination, tourists;
     inputFile >> start >> destination >> tourists;
 
     inputFile.close();
